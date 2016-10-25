1c3dddd07c61dc8f82c527326d1f09bbbea8ad00
Zhu Brocade Soar <soar@localhost.localdomain>
2016-10-25 20:45:29 +0800

    update

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <form action = "deal.php" method = "post">
 You can login with your port and password or <a herf = "104.160.32.39/nitmaker.usa/signup.php">sign up</a>
 <br/>
-Port:<input type = "int" name = "inputedPort"><br/>
+Port:<input type = "text" name = "inputedPort"><br/>
 Password:<input type = "text" name = "inputedPasswd"><br/>
-<input type = "submit">
+<input type = "submit" value = "Sign In">
 </form>
 </body>
 </html>
