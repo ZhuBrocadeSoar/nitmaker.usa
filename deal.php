b0c37a351853b3bc5a6a318cdf557e6e5a659e42
Zhu Brocade Soar <soar@localhost.localdomain>
2016-10-25 21:24:55 +0800

    add deal.php

---
+++ deal.php
@@ -0,0 +1,31 @@
+<?php
+$con = mysql_connect("localhost","soar","passwd");
+if(!con){
+    die('Could not connect:' . mysql_error());
+}
+mysql_select_db("ss_user_man", $con);
+//匹配port和passwd
+//从post获得用户输入
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $inputedPort = $_POST["inputedPort"];
+    $inputedPasswd = $_POST["inputedPasswd"];
+    $result = mysql_query("SELECT id,port,passwd FROM portPasswd");
+    while($row = mysql_fetch_array($result)){
+        if($row["port"] == $inputedPort){
+            if($row["passwd"] == $inputedPasswd){
+                $state = "success";
+                break;
+            }
+        }
+    }
+    if($state == "success"){
+        echo "Sueecssfully Sign in<br/>";
+    }else{
+        echo "Faild!<br/>";
+    }
+}else{
+    echo "Could not get datas from post<br/>";
+}
+
+
+?>
