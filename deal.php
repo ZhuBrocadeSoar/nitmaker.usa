018e07b3c5c7239ff4d9288bb6520bd58b81da8f
Zhu Brocade Soar <soar@localhost.localdomain>
2016-10-28 10:23:44 +0800

    update

--- deal.php
+++ deal.php
@@ -8,21 +8,27 @@ if(!con){
 mysql_select_db("ss_user_man", $con);
 //匹配port和passwd
 //从post获得用户输入
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $inputedPort = $_POST["inputedPort"];
     $inputedPasswd = $_POST["inputedPasswd"];
+    //根据用户输入的登录信息查询数据库
     $result = mysql_query("SELECT * FROM portPasswd WHERE port = $inputedPort");
-    while($row = mysql_fetch_array($result)){
-        if($row["passwd"] == $inputedPasswd){
+    if($row = mysql_fetch_array($result)){//port 匹配
+        if($row["passwd"] == $inputedPasswd){//passwd 匹配
             $state = "success";
-            if($row["adminLimit"] == 0){
+            $id = row["id"];
+            if($row["adminLimit"] == 0){//管理员权限检查
                 $state = "admin0";
             }
-            break;
+        }else{
+            $state = "errpasswd";
         }
+    }else{
+        $state = "errport";
     }
     switch($state){
-    case "success":case "admin0";
+    case "success":case "admin0":
         echo "<p>Sueecssfully Sign in</p>";
         echo "<p>Select a function</p>";
         break;
@@ -30,16 +36,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         echo "<p>Faild to Sign in</p>";
         break;
     }
+    echo "<form action = 'viewPortPasswdTable.php' method = 'post'>";
     switch($state){
+    case "success":
+        echo "<input type 'submit' value = 'Get SS infomation'>";
     case "admin0":
-        echo "<form action = 'viewPortPasswdTable.php' method = 'post'>";
         echo "<input type = 'submit' value = 'View the portPasswd table'>";
-        echo "<input type = 'hidden' name = 'state' value = '$state'>";
-        echo "</form>";
         break;
     default:
         break;
     }
+    echo "<input type = 'hidden' name = 'state' value = '$state'>";
+    echo "<input type = 'hidden' name = 'id' value = $id >";
+    echo "</form>";
 }else{
     echo "Could not get datas from post<br/>";
 }
